0737221ffcf7c8db61365f29b58d4d296e503f3a
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2023-04-01 19:23:36 +0300

    added time time line

--- invite_for_a_break/lib/src/screens/main/set_timeline.dart
+++ invite_for_a_break/lib/src/screens/main/set_timeline.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:invite_for_a_break/src/bloc/profile/profile_cubit.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class SetTimeLineView extends StatelessWidget {
   const SetTimeLineView({Key? key}) : super(key: key);
@@ -10,20 +11,23 @@ class SetTimeLineView extends StatelessWidget {
     return BlocBuilder<ProfileCubit, ProfileNavState>(
         builder: (context, state) {
       return Scaffold(
-        appBar: AppBar(
-          // back button
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              context.read<ProfileCubit>().gotoMain();
-            },
+          appBar: AppBar(
+            // back button
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {},
+            ),
+            title: Text("Set Timeline"),
           ),
-          title: Text("Set Timeline"),
-        ),
-        body: Center(
-          child: Text("Set Timeline"),
-        ),
-      );
+          body: SfCalendar(
+            view: CalendarView.day,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // open a dialog bloc with start time and end time
+            },
+            child: Icon(Icons.add),
+          ));
     });
   }
 }
